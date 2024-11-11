d63bba6f0f78feb01ef79599c5629d2087ca31d4
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-11 00:48:00 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,42 @@
-#include <iostream>
 #include "Player.h"
 using namespace std;
 
 Player::Player(double money) : balance(money) {}
-void Player::receiveCard(Card newCard) {
-	// TODO
+
+void Player::receiveCard(Deck& deck, int index) {
+	Card newCard = deck.getCard(index);
+	hand.push_back(newCard);
+}
+
+Card Player::getCard(int index) const {
+	return hand.at(index);
+}
+
+void Player::fold() {
+	balance -= bet;
+	cout << "You have folded. Your bet of $" << bet << " has been lost." << endl;
+}
+
+void Player::modifyBalance(double money, int operation) {
+	try {
+		if (operation == 1) {
+			balance += money;
+		}
+		else if (operation == 2) {
+			balance -= money;
+			if (balance < 0) {
+				balance = 0;
+			}
+		}
+		else {
+			throw("Invalid operation.");
+		}
+	}
+	catch (string x) {
+		cout << x << "\nPlease input either '1' or '2' for addition or subtraction respectively." << endl;
+	}
+}
+
+int Player::getBalance() const {
+	return balance;
 }
