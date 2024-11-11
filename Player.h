dc9310ca47a9d95f2ce774efecfb90bc59c9dd02
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-11 00:47:30 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <cstdlib>
 #include "Card.h"
 #include "Deck.h"
 using namespace std;
@@ -8,13 +9,13 @@ class Player {
 private:
 	vector<Card> hand;
 	double balance;
+	double bet;
 
 public:
-	Player(double money);
+	Player(double money = 200);
 	void receiveCard(Deck& deck, int index);
 	Card getCard(int index) const;
 	void fold();
-	void modifyBalance(double money);
+	void modifyBalance(double money, int operation);
 	int getBalance() const;
-	void savePlayerInfo();
 };
