073652f7b9c2614ed608e315ddaecbdb8bb933d2
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-13 04:30:47 -0700

    Update Card.h

--- Card.h
+++ Card.h
@@ -3,36 +3,40 @@
 
 using namespace std;
 class Card {
-    private:
-        string suite;
-        int rank;
-    public:
-        int index;
-        Card(string suite, int rank) : suite(suite), rank(rank) {}
-        Card(){
-            suite = suite;
-            rank = rank;
+private:
+    string suite;
+    int rank;
+public:
+    int index;
+    Card(string suite, int rank) : suite(suite), rank(rank) {}
+    Card() {
+        suite = suite;
+        rank = rank;
+    }
+    friend ostream& operator<<(ostream& os, const Card& card) {
+        string rankStr;
+        if (card.rank == 1) {
+            rankStr = "Ace";
+            os << rankStr << " of " << card.suite;
         }
-        friend ostream& operator<<(ostream& os, const Card& card) {
-            string rankStr;
-            if (card.rank == 1) {
-                rankStr = "Ace";
-                os << rankStr << " of " << card.suite;
-            } else if (card.rank == 11) {
-                rankStr = "Jack";
-                os << rankStr << " of " << card.suite;
-            } else if (card.rank == 12) {
-                rankStr = "Queen";
-                os << rankStr << " of " << card.suite;
-            } else if (card.rank == 13) {
-                rankStr = "King";
-                os << rankStr << " of " << card.suite;
-            } else {
-                os << card.rank << " of " << card.suite;
-            }
-            return os;
+        else if (card.rank == 11) {
+            rankStr = "Jack";
+            os << rankStr << " of " << card.suite;
         }
-        string getSuite() const { return suite; }
-        int getRank() const { return rank; }
+        else if (card.rank == 12) {
+            rankStr = "Queen";
+            os << rankStr << " of " << card.suite;
+        }
+        else if (card.rank == 13) {
+            rankStr = "King";
+            os << rankStr << " of " << card.suite;
+        }
+        else {
+            os << card.rank << " of " << card.suite;
+        }
+        return os;
+    }
+    string getSuite() const { return suite; }
+    int getRank() const { return rank; }
 
 };
