c62f5308bc717fe8acb78bb70898aec542b04ce6
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-13 04:28:54 -0700

    Update User.cpp

---
+++ User.cpp
@@ -0,0 +1,79 @@
+#include "User.h"
+using namespace std;
+
+User::User(string name, string fileName) : Player() {
+	bool validName = true;
+	string line;
+	string tempName;
+	ifstream file(fileName);
+	if (!file) {
+		cout << "Error opening the file. Please retype the file name." << endl;
+		return;
+	}
+	else {
+		while (getline(file, line)) {
+			istringstream stream(line);
+			if (stream >> tempName) {
+				if (tempName == name) {
+					validName = false;
+					file.seekg(0);
+				}
+			}
+		}
+
+		string newName;
+		while (!validName) {
+			cout << "That name already exists. Please select another name." << endl;
+			cin >> newName;
+			while (getline(file, line)) {
+				istringstream stream(line);
+				if (stream >> tempName) {
+					if (tempName == newName) {
+						file.seekg(0);
+						continue;
+					}
+				}
+			}
+		}
+		
+		if (validName) {
+			this->name = name;
+		}
+		else {
+			this->name = newName;
+		}
+	}
+}
+
+void User::saveUserInfo(string fileName) {
+	ofstream file(fileName, ios::app);
+	file << name << " " << getBalance << "\n";
+	file.close();
+}
+
+void User::getUserInfo(string fileName, string inputName) {
+	bool userFound = false;
+	string tempName;
+	double tempBalance;
+	string line;
+	ifstream file(fileName);
+	if (!file) {
+		cout << "Error opening the file. Please retype the file name." << endl;
+		return;
+	}
+	else {
+		while (getline(file, line)) {
+			istringstream stream(line);
+			if (stream >> tempName >> tempBalance) {
+				if (tempName == inputName) {
+					this->name = inputName;
+					setBalance(tempBalance);
+					userFound = true;
+				}
+			}
+		}
+
+		cout << "Success! Welcome back to the casino " << inputName << "." << endl;
+		file.close();
+	}
+}
