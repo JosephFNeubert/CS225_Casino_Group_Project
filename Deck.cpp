4388ba91fd34dbac93ab339272315a32568d9e2a
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-13 10:15:33 -0700

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -3,13 +3,27 @@
 #include <cstdlib>
 #include "Card.h"
 #include "Deck.h"
-
 using namespace std;
 
-    void Deck::shuffleDeck() {
-        srand(time(0));
-        for (int i = deck.size() - 1; i > 0; --i) {
-            int j = rand() % (i + 1);
-            swap(deck[i], deck[j]);
+Deck::Deck() {
+    string suites[] = { "Hearts", "Diamonds", "Spades", "Clubs" };
+
+    for (const string& suite : suites) {
+        for (int rank = 1; rank <= 13; ++rank) {
+            deck.push_back(Card(suite, rank));
         }
     }
+}
+
+Card Deck::getCard(int index) const {
+    return deck[index];
+
+}
+
+void Deck::shuffleDeck() {
+    srand(time(0));
+    for (int i = deck.size() - 1; i > 0; --i) {
+        int j = rand() % (i + 1);
+        swap(deck[i], deck[j]);
+    }
+}
