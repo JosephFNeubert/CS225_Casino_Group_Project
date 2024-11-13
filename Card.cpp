8bef8ee8046fac97401639b5a1b0c96fec273905
Joseph Neubert <95585229+JosephFNeubert@users.noreply.github.com>
2024-11-13 10:23:29 -0700

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -2,7 +2,14 @@
 #include <ctime>
 #include <cstdlib>
 #include "Card.h"
-
 using namespace std;
 
+Card::Card(string suite, int rank) : suite(suite), rank(rank) {}
+Card::Card() {
+    suite = suite;
+    rank = rank;
+}
 
+string Card::getSuite() const { return suite; }
+
+int Card::getRank() const { return rank; }
